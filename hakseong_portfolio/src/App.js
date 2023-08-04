f751d5d152b563860076b16bb82f57fe8440f972
진학성 <132454951+HarryJin12@users.noreply.github.com>
2023-08-04 09:31:39 +0900

    test

--- hakseong_portfolio/src/App.js
+++ hakseong_portfolio/src/App.js
@@ -3,22 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          진학성 포트폴리오
-        </a>
-      </header>
-    </div>
+   <div>
+<h1> 진학성 포트폴리오 입니다.</h1>
+
+   </div>
   );
 }
 
