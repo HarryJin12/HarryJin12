9110bbfa18782f2e4de2ed081768d4201bb15c52
진학성 <132454951+HarryJin12@users.noreply.github.com>
2023-08-04 12:46:25 +0900

    error

--- hakseong_portfolio/src/index.js
+++ hakseong_portfolio/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import App from './components/Main';
+import App from './App';
+// import App from './components/Main';
 import { BrowserRouter , Route, Routes } from "react-router-dom";
 
 import reportWebVitals from './reportWebVitals';
