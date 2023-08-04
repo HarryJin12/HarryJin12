01a31415a2960d7590978055c9c18c4b7241df5f
진학성 <132454951+HarryJin12@users.noreply.github.com>
2023-08-04 10:46:05 +0900

    test123

--- hakseong_portfolio/src/App.js
+++ hakseong_portfolio/src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 
 function App() {
   return (
-   <div>
-<h1> 진학성 포트폴리오 입니다.</h1>
+   <div/>
 
-   </div>
+   
   );
 }
 
---
+++ hakseong_portfolio/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+    <h1> 진학성 포트폴리오 입니다.</h1>
+       </div>
+  )
+}
+
+export default Main
