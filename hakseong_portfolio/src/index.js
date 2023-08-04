8d58a7243dc943ad6c93e7d93922a8085e9d3c31
진학성 <132454951+HarryJin12@users.noreply.github.com>
2023-08-04 12:32:54 +0900

    HashRouter

--- hakseong_portfolio/src/index.js
+++ hakseong_portfolio/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // basename={process.env.PUBLIC_URL}
   //  basename={process.env.PUBLIC_URL} 적어줘야 한다.
-  <HashRouter>
+  <HashRouter basename = "/">
     <App />
   </HashRouter>
   
